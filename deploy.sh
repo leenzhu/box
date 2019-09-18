1955854a1bffea86026b7f70015df76b501dfeb1
leen <i@leenzhu.com>
2019-09-18 14:30:08 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+change_apt_source() {
+	echo todo
+}
+
+change_python_source() {
+	echo todo
+}
+
+install_packages() {
+    packages=(
+    lrzsz
+    git
+    curl
+    vim
+    tmux
+    htop
+    tree
+    nginx
+    sqlite
+    python3
+    python3-pip
+    golang
+    )
+    apt update
+    for pkg in ${packages[@]};do
+        apt install -y $pkg
+    done
+}
+
+install_tmux_rc() {
+    # git clone tmuxrc
+    git clone https://github.com/leenzhu/tmux.d.git ~/.tmux.d
+    ln -s ~/.tmux.d/tmux.conf ~/.tmux.conf
+
+    echo run-shell ~/.tmux.d/better-mouse/scroll_copy_mode.tmux >> ~/.tmux.conf
+}
+
+install_oh_my_zsh() {
+    bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    chsh -s /bin/zsh
+}
+
+install_vim_rc() {
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    git clone https://github.com/leenzhu/vim.d.git ~/.vim.d
+    ln -s ~/.vim.d/vimrc ~/.vimrc
+}
+
+install_tmux_rc
+install_vim_rc
